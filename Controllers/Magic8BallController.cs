fb36a3f6355515b95a1c4a7906ea1e7ceb3b26da
Pedro Castaneda <petecast76@gmail.com>
2022-10-27 12:01:58 -0700

    First Commit Magic8Ball EP

---
+++ Controllers/Magic8BallController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using castanedaPMagic8BallEndpoint.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace castanedaPMagic8BallEndpoint.Controllers
+{
+    [Route("[controller]")]
+    public class Magic8BallController : Controller
+    {
+        Random r = new Random();
+        string[] magicBallAnswers = {"It is cerain.", "It is decidedly so.", "Without a doubt.", "Yes definitely.", "You may rely on it", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."};
+        [HttpGet("Magic8Ball")]
+        public string MagicAnswer(MagicModel user)
+        {
+            int randomAnswer = r.Next(0, magicBallAnswers.Length);
+            return $"{magicBallAnswers[randomAnswer]}";
+        }
+    }
+}
---
+++ Models/MagicModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace castanedaPMagic8BallEndpoint.Models
+{
+    public class MagicModel
+    {
+        public string input {get; set;}
+
+        public MagicModel(){}
+    }
+}
